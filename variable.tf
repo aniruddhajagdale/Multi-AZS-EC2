f6ff191ae7a3de633bb895ff7602ff1b94be8a10
Jagdale <aniruddha.jagdale@dxc.com>
2020-04-30 14:41:40 +0530

    Removed credentials

--- variable.tf
+++ variable.tf
@@ -1,9 +1,4 @@
-variable "accessKey" {
-  default = "AKIAYKBH6HWZVORPMF6Y"
-}
-variable "secretKey" {
-  default = "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"
-}
+
 variable "region" {
   default = "ap-south-1"
 }
